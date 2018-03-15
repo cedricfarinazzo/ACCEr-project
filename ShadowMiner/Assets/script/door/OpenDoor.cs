d7548e16805e6d587b9a6417283fab3cbf343a54
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-03-15 18:47:19 +0100

    fix a lot of bugs : gravity door mass jump arch ...

--- ShadowMiner/Assets/script/door/OpenDoor.cs
+++ ShadowMiner/Assets/script/door/OpenDoor.cs
@@ -10,22 +10,27 @@ public class OpenDoor : MonoBehaviour {
     protected Animation animation;
     protected bool open = false;
 
+    private int timeout = 0;
+
 	// Use this for initialization
 	void Start () {
-        this.animation = this.gameObject.GetComponent<Animation>();
+        this.animation = this.gameObject.GetComponentInChildren<Animation>();
 	}
 
     // Update is called once per frame
     void Update()
     {
-
+        if (this.timeout > 0)
+        {
+            this.timeout--;
+        }
     }
 
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(this.touche))
+            if (Input.GetKeyDown(this.touche) && this.timeout == 0)
             {
                 if (!this.open)
                 {
@@ -35,6 +40,7 @@ public class OpenDoor : MonoBehaviour {
                 {
                     this.closedoor();
                 }
+                this.timeout = 50;
             }
         }
     }
