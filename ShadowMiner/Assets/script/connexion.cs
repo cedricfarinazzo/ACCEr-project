c61aaa2304716fe23685414123703b0f8faa999f
Unknown <antoine.claudel@epita.fr>
2018-04-23 20:34:19 +0200

    Ajout de la scène connexion
    id : pseudo
    mdp : password

---
+++ ShadowMiner/Assets/script/connexion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class connexion : MonoBehaviour {
+	public InputField pseudo;
+	public InputField password;
+
+	void Update ()
+	{
+		if (pseudo.text == "pseudo" && password.text == "password" && Input.GetKeyDown ("return")) {
+			Debug.Log ("Logged as guest");
+		
+		}
+		else {if (Input.GetKeyDown ("return")) {
+				Debug.Log ("Wrong password or pseudo"); }
+		}
+	}
+}
