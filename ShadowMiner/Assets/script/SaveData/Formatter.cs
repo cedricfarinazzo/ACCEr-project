1d2e4555b699ae2fe9dfc3d7f034ab5074355384
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-04-24 17:20:15 +0200

    Add SaveData

---
+++ ShadowMiner/Assets/script/SaveData/Formatter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace SaveData
+{
+    public static class Formatter {
+
+        public static string ToJson<T>(T elm)
+        {
+            if (elm == null)
+            {
+                return null;
+            }
+
+            string json = JsonConvert.SerializeObject(elm);
+            return json;
+        }
+
+        public static T ToObject<T>(string json)
+        {
+            if (json == null || json == "")
+            {
+                return default(T);
+            }
+
+            T obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+
+    }
+}
