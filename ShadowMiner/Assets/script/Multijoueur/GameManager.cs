085cca6735cd163d9acb3e35a38439faeb2fcc61
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-04-25 21:03:50 +0200

    add game manager on multiplayer scene

---
+++ ShadowMiner/Assets/script/Multijoueur/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Photon.MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Instantiate()
+    {
+        GameObject j = PhotonNetwork.Instantiate(player.name, this.transform.position + Vector3.up * 2, Quaternion.identity, 0);
+        j.GetComponent<PlayerController>().enabled = true;
+        j.GetComponent<CursorTurnHory>().enabled = true;
+        j.GetComponent<Entity>().enabled = true;
+        j.GetComponentInChildren<CursorTurnVerti>().enabled = true;
+        j.GetComponentInChildren<Camera>().enabled = true;
+        j.GetComponentInChildren<AudioListener>().enabled = true;
+    }
+}
