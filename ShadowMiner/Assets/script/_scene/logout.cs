e55df647fe9f9d09b186019ac3f35762909b781d
Unknown <antoine.claudel@epita.fr>
2018-04-25 18:34:36 +0200

    bouton logout

---
+++ ShadowMiner/Assets/script/_scene/logout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SMNetwork.Client;
+using UnityEngine.SceneManagement;
+
+public class logout : MonoBehaviour {
+
+	Client nouveauclient;
+	public Button logoutbutton;
+	// Use this for initialization
+	void Start () {
+		nouveauclient = new Client();
+		logoutbutton.onClick.AddListener(Logout);
+	}
+
+	public void Logout(){
+		nouveauclient.Logout ();
+		SceneManager.LoadScene ("connexion");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
