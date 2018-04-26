c39fba7f9940281674b42a8fb77269c14bf5e1f6
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-04-26 21:24:47 +0200

    corrige les violations de nom

---
+++ ShadowMiner/Assets/script/Parametre/Parametre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SaveData;
+
+namespace SMParametre
+{
+    [Serializable]
+    public class Parametre {
+
+	    public string Version = "0.2";
+		
+	    public Parametre()
+	    {
+	    }
+		
+	    public static void Load()
+	    {
+		    // sera appélé par beaucoup de script
+		    // recupère depuis SaveData les données sauvegardées et retourne un objet Parametre
+		    // key = "Parametre"
+	    }
+
+	    public void Save()
+	    {
+		    //enregistre grace à savedata
+		    // key = "Parametre"
+	    }
+    }
+
+}
+
