652aeea762cb161334bb05911568f7ae286eb3c0
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-04-28 21:34:38 +0200

    graphics parametre

--- ShadowMiner/Assets/script/Parametre/Parametre.cs
+++ ShadowMiner/Assets/script/Parametre/Parametre.cs
@@ -6,8 +6,49 @@ using SaveData;
 
 namespace SMParametre
 {
+    enum Frequency
+    {
+        Low = 30,
+        Hight = 60
+    }
+
+    enum GraphicsQuality
+    {
+        VeryLow = 0,
+        Low = 1,
+        Medium = 2,
+        Hight = 3,
+        VeryHight = 4,
+        Ultra = 5
+    }
+
     [Serializable]
     public class Parametre {
+
+        public static List<int[]> ResolutionList = new List<int[]>()
+        {
+            new int[2] {320, 200},
+            new int[2] {320, 240},
+            new int[2] {400, 300},
+            new int[2] {512, 384},
+            new int[2] {640, 400},
+            new int[2] {640, 480},
+            new int[2] {800, 600},
+            new int[2] {1024, 768},
+            new int[2] {1152, 864},
+            new int[2] {1280, 600},
+            new int[2] {1280, 720},
+            new int[2] {1280, 768},
+            new int[2] {1280, 800},
+            new int[2] {1280, 960},
+            new int[2] {1280, 1024},
+            new int[2] {1360, 768},
+            new int[2] {1366, 768},
+            new int[2] {1400, 1050},
+            new int[2] {1440, 900},
+            new int[2] {1600, 900},
+            new int[2] {1680, 1050},
+        };
 
 	    public string Version = "0.2";
 		public KeyCode Avancer = KeyCode.Z;
@@ -22,8 +63,8 @@ namespace SMParametre
 		public float Sensi = 1;
 		public int Quality = 5; // <= 5
 		public int Frequency = 60;
-		public int[] Resolution = {1650,1050};
-        public bool Windowed = true;
+		public int Resolution = 20;
+        public bool FullScreen = true;
 
 	    public Parametre()
 	    {
@@ -31,13 +72,25 @@ namespace SMParametre
 		
 	    public static Parametre Load()
 	    {
-            return SaveData.SaveData.GetObject<Parametre>("Parametre");
+            Parametre p = SaveData.SaveData.GetObject<Parametre>("Parametre");
+            if (p == null)
+            {
+                return new Parametre();
+            }
+            return p;
 	    }
 
 	    public void Save()
 	    {
-            SaveData.SaveData.SaveObject<Parametre>("Parametre", this);
+            SaveData.SaveData.SaveObject("Parametre", this);
 	    }
+
+        public void Apply()
+        {
+            QualitySettings.SetQualityLevel(this.Quality, true);
+            Screen.SetResolution(ResolutionList[Resolution][0],
+                ResolutionList[Resolution][1], FullScreen, Frequency);
+        }
     }
 
 }
---
+++ ShadowMiner/Assets/script/Parametre/ParametreGraphics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParametreGraphics : MonoBehaviour {
+
+    protected SMParametre.Parametre param;
+    [SerializeField]
+    protected Button Apply;
+    [SerializeField]
+    protected Dropdown Quality;
+    [SerializeField]
+    protected Dropdown Freq;
+    [SerializeField]
+    protected Dropdown Resolu;
+    [SerializeField]
+    protected Toggle fullscreen;
+
+	// Use this for initialization
+	void Start () {
+        param = SMParametre.Parametre.Load();
+        Freq.value = param.Frequency == 60 ? 1 : 0;
+        Quality.value = param.Quality;
+        Resolu.value = param.Resolution;
+        fullscreen.isOn = param.FullScreen;
+        Apply.onClick.AddListener(Save);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Save()
+    {
+        param.Quality = Quality.value;
+        param.Resolution = Resolu.value;
+        param.FullScreen = fullscreen.isOn;
+        param.Frequency = Freq.value == 0 ? 30 : 60;
+        param.Apply();
+        param.Save();
+    }
+}
