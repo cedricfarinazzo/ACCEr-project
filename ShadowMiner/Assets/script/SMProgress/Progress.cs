e9b7a95c73351ace3824bfd9e92da5e6f499bd3e
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-04-30 00:23:06 +0200

    add progress script

---
+++ ShadowMiner/Assets/script/SMProgress/Progress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SMProgress
+{
+    [Serializable]
+    public class Progress
+    {
+
+        private ProgressMulti multi;
+        private ProgressSolo solo;
+
+        public ProgressMulti Multi
+        {
+            get
+            {
+                return multi;
+            }
+
+            set
+            {
+                multi = value;
+            }
+        }
+
+        public ProgressSolo Solo
+        {
+            get
+            {
+                return solo;
+            }
+
+            set
+            {
+                solo = value;
+            }
+        }
+
+        public Progress()
+        {
+            solo = new ProgressSolo();
+            multi = new ProgressMulti();
+        }
+
+        public override string ToString()
+        {
+            return Solo.ToString() + " " + Multi.ToString();
+        }
+
+        public static Progress Parse(string text)
+        {
+            try
+            {
+                string[] data = text.Split(' ');
+                Progress p = new Progress();
+                p.Solo = ProgressSolo.Parse(data[0]);
+                p.Multi = ProgressMulti.Parse(data[1]);
+                return p;
+            }
+            catch(Exception)
+            {
+                return new Progress();
+            }
+        }
+    }
+}
---
+++ ShadowMiner/Assets/script/SMProgress/ProgressMulti.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SMProgress
+{
+    [Serializable]
+    public class ProgressMulti
+    {
+
+        private int nbWin = 0;
+
+        public int NbWin
+        {
+            get
+            {
+                return nbWin;
+            }
+
+            set
+            {
+                nbWin = value;
+            }
+        }
+
+        public ProgressMulti()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return NbWin.ToString();
+        }
+
+        public static ProgressMulti Parse(string text)
+        {
+            try
+            {
+                int i = int.Parse(text);
+                ProgressMulti m = new ProgressMulti();
+                m.NbWin = i;
+                return m;
+            }
+            catch(Exception)
+            {
+                return new ProgressMulti();
+            }
+        }
+    }
+}
