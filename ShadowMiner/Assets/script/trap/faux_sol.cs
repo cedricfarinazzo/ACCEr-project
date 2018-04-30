c5f8692de370d4e9853db3c84add144899877a24
Unknown <antoine.claudel@epita.fr>
2018-04-30 20:47:32 +0200

    fauxsol+script+vraidoublewall

--- ShadowMiner/Assets/script/trap/faux_sol.cs
+++ ShadowMiner/Assets/script/trap/faux_sol.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class faux_sol : MonoBehaviour {
 
+	public GameObject fauxsol;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void OnTriggerEnter(Collider col){
+		GameObject jeuobjet = col.gameObject;
+		if (jeuobjet.tag == "Player") {
+			fauxsol.SetActive (false);
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 		
