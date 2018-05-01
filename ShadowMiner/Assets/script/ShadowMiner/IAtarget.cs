756b1ab2cb5a1ebcf8a46ec575aa3584b9eaa56c
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-05-01 16:57:31 +0200

    new shadow miner

---
+++ ShadowMiner/Assets/script/ShadowMiner/IAtarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAtarget : MonoBehaviour {
+
+	[SerializeField]
+	private NavMeshAgent agent;
+
+	protected Vector3 target;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		target = this.gameObject.transform.position;
+		agent.SetDestination(target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(target);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("target: on");
+			target = other.transform.position;
+		}
+	}
+
+	/*
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			target = this.gameObject.transform.position;
+		}
+	}*/
+}
