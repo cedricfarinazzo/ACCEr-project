ed8918179bccfa814cb05cf238d375759a6b8d7c
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-05-01 02:31:52 +0200

    test nav mesh

---
+++ ShadowMiner/Assets/script/NavAgentController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentController : MonoBehaviour
+{
+
+	[SerializeField] protected Transform target;
+	
+	void Start ()
+	{
+		NavMeshAgent agent = this.gameObject.GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
+	}
+}
