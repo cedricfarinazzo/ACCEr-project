0ab7e636122df3890819e39bca9727d7d37a441e
Unknown <cedric.farinazzo@gmail.com>
2018-05-02 15:21:26 +0200

    patch some bugs

---
+++ ShadowMiner/Assets/script/PauseMenu/PauseManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : Photon.MonoBehaviour
+{
+
+    [SerializeField] protected GameObject canvas;
+    [SerializeField] protected Button Resume;
+    [SerializeField] protected Button BackMenu;
+
+    [SerializeField]
+    private bool visible = false;
+
+    private KeyCode key = KeyCode.Escape;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        /*
+        if (visible && Input.GetKey(key))
+        {
+            ClickResume();
+        }*/
+        if (Input.GetKeyDown(key))
+        {
+            visible = !visible;
+        }
+
+        if (visible)
+        {
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            canvas.SetActive(true);
+            Resume.onClick.AddListener(ClickResume);
+            BackMenu.onClick.AddListener(ClickBackMenu);
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1f;
+            canvas.SetActive(false);
+        }
+    }
+
+    public void ClickResume()
+    {
+        visible = false;
+    }
+
+    public void ClickBackMenu()
+    {
+        SaveData.SaveData.SaveString("niveaux_solo", name);
+        SceneManager.LoadScene("loading");
+    }
+}
