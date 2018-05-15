baba61b9b153c478cdd3e9ef8d81e747e05082eb
antoine <antoine.claudel@epita.fr>
2018-05-15 12:40:47 +0200

    tentative degats flamme

---
+++ ShadowMiner/Assets/script/trap/dégats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dégats : Trap {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (this.actual_time > 0)
+		{
+			this.actual_time--;
+		}
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Attack(other.gameObject);
+		}
+	}
+}
