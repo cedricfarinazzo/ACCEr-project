9ea55e45689e9863ada06edeb98c4448bdc952a9
antoine <antoine.claudel@epita.fr>
2018-06-05 23:36:38 +0200

    Script clé fonctionnel
    cimer albert

---
+++ ShadowMiner/Assets/script/door/getkey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getkey : MonoBehaviour {
+
+	protected KeyCode touche;
+
+	void Start()
+	{
+		SMParametre.Parametre param = SMParametre.Parametre.Load ();
+		touche = param.Key ["Interact"];
+	}
+	
+	public void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			if (Input.GetKeyDown(this.touche))
+				{
+				GameObject commetuveux = GameObject.Instantiate (new GameObject ());
+				commetuveux.name = "key";
+				commetuveux.transform.SetParent (other.gameObject.transform);
+				this.gameObject.SetActive (false);
+				}
+		}
+	}
+}
--- ShadowMiner/Assets/script/door/keydoorscript.cs
+++ ShadowMiner/Assets/script/door/keydoorscript.cs
@@ -3,31 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class keydoorscript : OpenDoor {
-
-	protected bool havekey = true;
-	protected GameObject _key;
-
-	// Use this for initialization
-	void Start () {
-		SMParametre.Parametre param = SMParametre.Parametre.Load ();
-		touche = param.Key ["Interact"];
-		this._g = this.gameObject;
-		this.animation = this.gameObject.GetComponentInChildren<Animation>();
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (this.timeout > 0)
-		{
-			this.timeout--;
-		}
-	}
-
-	public new void OnTriggerStay(Collider other)
+	
+	public void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
+			Transform[] listedetransform = other.gameObject.GetComponentsInChildren<Transform> ();
+			bool havekey = false;
+			foreach (Transform transfo in listedetransform) {
+					if(transfo.gameObject.name == "key")
+					{
+						havekey = true;
+					}
+			}
 			if (Input.GetKeyDown(this.touche) && this.timeout == 0 && havekey)
 			{
 				if (!this.open)
