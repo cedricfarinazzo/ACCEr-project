f66d08fc14fb199fb315ac50f4d9a59aa4c4ef5f
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-06-06 14:42:53 +0200

    enable grille et levier

---
+++ ShadowMiner/Assets/script/leviergrille/DownLevier.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownLevier : MonoBehaviour {
+
+    protected KeyCode touche;
+    [SerializeField]
+    protected GameObject bar;
+    protected bool Down = false;
+    protected int progress = 50;
+
+    // Use this for initialization
+    void Start () {
+        SMParametre.Parametre param = SMParametre.Parametre.Load();
+        touche = param.Key["Interact"];
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !Down)
+        {
+            if (Input.GetKeyDown(this.touche))
+            {
+                GameObject grille = GameObject.Instantiate(new GameObject());
+                grille.name = "grille";
+                grille.transform.SetParent(other.gameObject.transform);
+                Down = true;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (Down)
+        {
+            if (progress == 0)
+            {
+                gameObject.GetComponent<DownLevier>().enabled = false;
+            }
+            else
+            {
+                bar.transform.Rotate(1, 0, 0);
+                progress--;
+            }
+
+        }
+    }
+}
---
+++ ShadowMiner/Assets/script/leviergrille/upgrille.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upgrille : MonoBehaviour {
+
+    protected KeyCode touche;
+    protected bool Up = false;
+    protected int progress = 55;
+
+    // Use this for initialization
+    void Start()
+    {
+        SMParametre.Parametre param = SMParametre.Parametre.Load();
+        touche = param.Key["Interact"];
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !Up)
+        {
+            if (Input.GetKeyDown(this.touche))
+            {
+                Transform[] listedetransform = other.gameObject.GetComponentsInChildren<Transform>();
+                bool havekey = false;
+                foreach (Transform transfo in listedetransform)
+                {
+                    if (transfo.gameObject.name == "grille")
+                    {
+                        GameObject.Destroy(transfo.gameObject);
+                        havekey = true;
+                    }
+                }
+                Up = havekey;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (Up)
+        {
+            if (progress == 0)
+            {
+                gameObject.GetComponent<upgrille>().enabled = false;
+            }
+            else
+            {
+                gameObject.transform.Translate(0, 0.1f, 0);
+                progress--;
+            }
+
+        }
+    }
+}
